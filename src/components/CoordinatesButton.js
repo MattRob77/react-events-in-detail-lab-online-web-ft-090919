3be826b1de20ffb8990f1aab365a81202d4cd96d
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-23 13:57:22 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -5,7 +5,7 @@ export default class CoordinatesButton extends Component {
 
   coordinatesButtonHandler = (e) => {
     let array = [event.clientX, event.clientY]
-    this.props 
+    this.props.On
   }
 }
 
