4dc4400f808a6d4b478a63e4732b9cac79ccbb7a
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-23 13:56:05 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 
 export default class CoordinatesButton extends Component {
 
-  coordinatesButtonHandler= 
+  coordinatesButtonHandler = (e) => {
+    
+  }
 }
 
 
