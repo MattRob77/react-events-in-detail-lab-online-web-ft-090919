9ec4eb905539b011cf35b1f4ddb8ac468037ef35
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-23 14:06:30 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -3,7 +3,7 @@ import React, { Component} from 'react'
 
 export default class DelayedButton extends Component {
   
-  delayedButtonHandler = (e) => {
+  delayedButtonHandler = (event) => {
     event.persist()
     
    setTimeout(() => {
